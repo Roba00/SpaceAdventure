13d0c024f700632f24ceb4fdf476fde3aa8ff299
Roba Abbajabal <32786155+abbajabalroba@users.noreply.github.com>
2019-07-20 21:02:54 -0500

    Player , Tilemap and Audio Improvements Added enemy collision detection, added player invincibility frames/animation, added tilemap collision detection, improved background music by speeding up the melody, tinted the ground tilemap red.

--- Assets/Scripts/Player/Control/PlayerFlyControl.cs
+++ Assets/Scripts/Player/Control/PlayerFlyControl.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class PlayerFlyControl : MonoBehaviour
 {
+    public SpriteRenderer[] playerSprites;
     public Rigidbody2D playerRb;
     private float force;
     private float incrementalForce;
     private float maxForce;
     private float normalSpeed;
     private float slowSpeed;
-    private bool isBraking = false;
+    private bool isBraking;
+    private bool isInvincible;
+    private int lives;
 
     void Start()
     {
+        playerSprites = gameObject.GetComponentsInChildren<SpriteRenderer>();
         force = 0;
         incrementalForce = 2f;
         maxForce = 18f;
         normalSpeed = 0.1f;
         slowSpeed = 0.04f;
+        isBraking = false;
+        isInvincible = false;
+        lives = 3;
     }
 
     void Update()
@@ -26,6 +33,22 @@ public class PlayerFlyControl : MonoBehaviour
         BrakeControl();
         FlyControl();
         HorizantalMovementControl();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy" && !isInvincible)
+        {
+            StartCoroutine(Invivility());
+        }
+        if (col.gameObject.tag == "Energy")
+        {
+            EnergyCollide(col.gameObject);
+        }
+        if (col.gameObject.tag == "Upgrade")
+        {
+            UpgradeCollide(col.gameObject);
+        }
     }
 
     void HorizantalMovementControl()
@@ -83,5 +106,52 @@ public class PlayerFlyControl : MonoBehaviour
         }
 
         playerRb.AddForce(Vector2.up * force);
+    }
+
+    IEnumerator Invivility()
+    {
+        isInvincible = true;
+        for (int j = 0; j < 3; j++)
+        {
+            for (int i = 0; i < playerSprites.Length; i++)
+            {
+                Color tempInvisibility = playerSprites[i].color;
+                tempInvisibility.a = 0.25f;
+                tempInvisibility.b = 0f;
+                tempInvisibility.g = 0f;
+                playerSprites[i].color = tempInvisibility;
+            }
+            yield return new WaitForSeconds(0.2f);
+            for (int i = 0; i < playerSprites.Length; i++)
+            {
+                Color tempVisibility = playerSprites[i].color;
+                tempVisibility.a = 1f;
+                tempVisibility.b = 255f;
+                tempVisibility.g = 255f;
+                playerSprites[i].color = tempVisibility;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+        isInvincible = false;
+    }
+
+    void EnergyCollide(GameObject energy)
+    {
+
+    }
+
+    void UpgradeCollide(GameObject upgrade)
+    {
+
+    }
+    
+    void Damage(int amount)
+    {
+
+    }
+
+    void Death()
+    {
+
     }
 }
